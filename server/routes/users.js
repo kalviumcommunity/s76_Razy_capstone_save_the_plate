90afcaf4b5791e3b56f2a996bbbc61cf3d0dfdbb
R-azy <muhammedrazy1798@gmail.com>
2025-05-13 16:39:17 +0530

    done put api

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const { User } = require('../models/user');
 const bcrypt = require('bcrypt');
 
-
-
+// POST endpoint for creating a user
 router.post("/", async (req, res) => {
     try {
-        const { error } =req.body;
+        const { error } = req.body;
         if (error)
-            return res.status(400).send({ meessage: error.details[0].message });
+            return res.status(400).send({ message: error.details[0].message });
         const user = await User.findOne({ email: req.body.email });
         if (user)
-            return res.status(409).send({ message: "User already exist!" });
+            return res.status(409).send({ message: "User already exists!" });
         
         const salt = await bcrypt.genSalt(Number(process.env.SALT));
         const hashPassword = await bcrypt.hash(req.body.password, salt);
@@ -21,5 +20,32 @@ router.post("/", async (req, res) => {
     } catch (error) {
         res.status(500).send({ message: "Internal server error!" });
     }
-})
+});
+
+// PUT endpoint for updating user details
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { firstName, lastName, email, password } = req.body;
+
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).send({ message: "User not found!" });
+        }
+
+        if (firstName) user.firstName = firstName;
+        if (lastName) user.lastName = lastName;
+        if (email) user.email = email;
+        if (password) {
+            const salt = await bcrypt.genSalt(Number(process.env.SALT));
+            user.password = await bcrypt.hash(password, salt);
+        }
+
+        await user.save();
+        res.status(200).send({ message: "User updated successfully!", user });
+    } catch (error) {
+        res.status(500).send({ message: "Internal server error!", error });
+    }
+});
+
 module.exports = router;
